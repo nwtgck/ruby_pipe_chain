7e0e50ffa36bb6b2d4d0a222884700ed7101317e
Ryo Ota <nwtgck@gmail.com>
2015-08-03 00:13:22 +0900

    [change] Change demo

--- demo.rb
+++ demo.rb
@@ -1,11 +1,13 @@
 require 'pipe_chain'
 
+# Using pipe
 puts "hello" | :length
 #=> 5
 
+# Using pipe with parameter
 puts [1, 2, 3] *  5 | :join['-']
 #=> "1-2-3-1-2-3-1-2-3-1-2-3-1-2-3"
 
-
-[1, 2, 3] *  5 | :join['-'] ^ :puts
-#=> "1-2-3-1-2-3-1-2-3-1-2-3-1-2-3"
+# Using functional method
+"hello" | :length ^ :puts
+#=> 5
